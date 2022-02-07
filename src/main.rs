346519ca21d279ba060bace2f3c7b15971c1702a
Ivan Vasiunyk <i.vasiunyk@gmail.com>
2022-02-07 13:35:31 +0100

    Basic terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, it's me a terminal.");
+
+        if let Some(VirtualKeyCode::Escape) = ctx.key {
+            ctx.quitting = true
+        }
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crow")
+        .with_fullscreen(true)
+        .build()?;
+
+    main_loop(context, State {})
 }
