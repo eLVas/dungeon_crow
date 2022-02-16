d6eabed23bbe492cf4e61b5c2ecdfc04b555bc79
Ivan Vasiunyk <i.vasiunyk@gmail.com>
2022-02-16 14:00:46 +0100

    Add flavortext to gameover screen

--- src/main.rs
+++ src/main.rs
@@ -102,6 +102,25 @@ impl State {
     fn game_over(&mut self, ctx: &mut BTerm) {
         ctx.set_active_console(2);
         ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
+        ctx.print_color_centered(
+            4,
+            WHITE,
+            BLACK,
+            "Slain by a monster, your hero's journey has come to a premature end.",
+        );
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "The Amulet of Yala remains unclaimed, and your home town is not saved.",
+        );
+        ctx.print_color_centered(
+            8,
+            YELLOW,
+            BLACK,
+            "Don't worry, you can always try again with a new hero.",
+        );
+        ctx.print_color_centered(9, GREEN, BLACK, "Press NumEnter to play again.");
 
         if let Some(VirtualKeyCode::NumpadEnter) = ctx.key {
             self.restart()
